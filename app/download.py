f1af8ec9be559461e4ccece076f35661a29b467a
satorukadowaki <satoru.kadowaki@gmail.com>
2021-08-26 13:49:43 +0000

    2021-08-26 updated

--- app/download.py
+++ app/download.py
@@ -7,8 +7,8 @@ from urllib import request
 def main():
     domain = "https://www.pref.yamagata.jp"
     covid_files_path = [
-        "/documents/10045/060003_yamagata_covid19_patients_0824.csv",
-        "/documents/10045/060003_yamagata_covid19_test_people_0825.csv",
+        "/documents/10045/060003_yamagata_covid19_patients_0825.csv",
+        "/documents/10045/060003_yamagata_covid19_test_people_0826.csv",
     ]
     save_files_path = ["./master_data/csv/patients_org.csv", "./master_data/csv/tests_org.csv"]
 
