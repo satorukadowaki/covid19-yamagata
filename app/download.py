af2a12c08a023018ac4c6df3af55ec7e4a1e94e4
satorukadowaki <satoru.kadowaki@gmail.com>
2021-10-08 02:27:30 +0000

    download: 過去日遡りを行うように変更, 2021-10-08 updated

--- app/download.py
+++ app/download.py
@@ -16,15 +16,27 @@ def main():
         f"/documents/10045/060003_yamagata_covid19_patients_{pat_sfx}.csv",
         f"/documents/10045/060003_yamagata_covid19_test_people_{tst_sfx}.csv",
     ]
-    print(covid_files_path)
+
+    # -- 感染者数0だと過去に遡る必要があるので20日分くらい作っておく
+    for i in range(2, 20):
+        pat_sfx = now.subtract(days=i).strftime("%m%d")
+        covid_files_path.append(f"/documents/10045/060003_yamagata_covid19_patients_{pat_sfx}.csv")
+
     save_files_path = ["./master_data/csv/patients_org.csv", "./master_data/csv/tests_org.csv"]
 
     # -- download patients data
     for idx, path in enumerate(covid_files_path):
         url = domain + path
         print(f"Downloading...{url}")
-        file_data = request.urlopen(url).read()
-        save_path = save_files_path[idx]
+        try:
+            file_data = request.urlopen(url).read()
+        except:
+            print(f"Download Failed:{url}")
+            continue
+        if "patients" in path:
+            save_path = save_files_path[0]
+        else:
+            save_path = save_files_path[1]
         save_path_utf8 = save_path.replace("_org", "")
         with open(save_path, "wb") as f:
             f.write(file_data)
@@ -33,6 +45,10 @@ def main():
             with codecs.open(save_path_utf8, "w", "utf-8") as f_out:
                 for row in f_in:
                     f_out.write(row)
+
+        # -- 患者のデータは取れたらやめる
+        if "patients" in path:
+            break
 
 
 if __name__ == "__main__":
